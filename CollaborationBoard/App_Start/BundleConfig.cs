e2bdd52c656334eb1d222ac0df9e1d557bae540d
Ivan Chub <subsbox@hotmail.com>
2014-07-27 11:29:03 -0700

    new bundle for app scripts

--- CollaborationBoard/App_Start/BundleConfig.cs
+++ CollaborationBoard/App_Start/BundleConfig.cs
@@ -27,6 +27,8 @@ namespace CollaborationBoard
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app_scripts").IncludeDirectory("~/Scripts/App/", "*.js"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
