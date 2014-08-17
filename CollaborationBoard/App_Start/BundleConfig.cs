1c0bbd5f4cdd957630d780102d041219c53f0cb8
Ivan Chub <subsbox@hotmail.com>
2014-08-17 15:29:07 -0700

    fixed more patterns

--- CollaborationBoard/App_Start/BundleConfig.cs
+++ CollaborationBoard/App_Start/BundleConfig.cs
@@ -16,12 +16,12 @@ namespace CollaborationBoard
                         "~/Scripts/jquery-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.6.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
