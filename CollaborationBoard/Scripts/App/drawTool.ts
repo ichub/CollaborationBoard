e0acbf3b00eca58a2f26414ac7dfd75924c987cc
Ivan Chub <subsbox@hotmail.com>
2014-10-19 16:05:43 -0700

    added brians stuff

--- CollaborationBoard/Scripts/App/drawTool.ts
+++ CollaborationBoard/Scripts/App/drawTool.ts
@@ -37,6 +37,32 @@
         }
     }
 
+    private onMouseDragDraw(xCoords: Array<number>, yCoords: Array<number>): void {
+        var x = new Array(xCoords.length + 2);
+        var y = new Array(yCoords.length + 2);
+        x[0] = xCoords[0];
+        y[0] = yCoords[0];
+        x[1] = xCoords[0];
+        y[1] = yCoords[0];
+        for (var i = 1; i < xCoords.length + 1; i++) {
+            x[i] = xCoords[i - 1];
+            y[i] = yCoords[i - 1];
+        }
+        x[xCoords.length + 1] = xCoords[xCoords.length - 1];
+        y[yCoords.length + 1] = yCoords[yCoords.length - 1];
+        for (var i = 1; i < x.length; i++) {
+            this.context.beginPath();
+            this.context.moveTo(x[i], y[i]);
+            var controlPointX1 = (x[i] + x[i + 1] - x[i - 1]) / 4;
+            var controlPointY1 = (y[i] + y[i + 1] - y[i - 1]) / 4;
+            var controlPointX2 = (x[i + 1] + x[i] - x[i + 2]) / 4;
+            var controlPointY2 = (y[i + 1] + y[i] - y[i + 2]) / 4;
+            this.context.bezierCurveTo(controlPointX1, controlPointY1, controlPointX2, controlPointY2, x[i + 1], y[i + 1]);
+            this.context.stroke();
+            this.context.closePath();
+        }
+    }
+
     private drawSmoothPath(path: Array<Point>): void {
         for (var i = 0; i < path.length; i++) {
             this.context.fillRect(path[i].x, path[i].y, 5, 5);
