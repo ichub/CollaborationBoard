807a92597491b7deaa6c3305d7eb286f4ba80680
Ivan Chub <ivansergchub@gmail.com>
2015-05-04 19:57:09 -0700

    removed 'contact' page

--- CollaborationBoard/Controllers/HomeController.cs
+++ CollaborationBoard/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace CollaborationBoard.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
